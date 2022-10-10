cb0f6d11ea90f6abf238bd17e517d008745d24f7
ntwali edson <ntwaliedson9@gmail.com>
2022-10-10 17:42:30 +0200

    fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Roboto", "arial"],
+      },
       colors: {
-        "bg-rgba": "rgba(255, 255, 255, 0.59)",
-        "bg-rgba-2": "rgba(215, 152, 225, 0.15)",
-        "bg-rgba-3": "rgba(97, 153, 237, 0.26)",
-        "bg-rgba-4": "rgba(255, 255, 255, 0.69)",
+        primary: "#1D2130",
       },
     },
   },
